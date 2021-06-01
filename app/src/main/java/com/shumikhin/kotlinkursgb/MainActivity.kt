63f47369e1e287b835245fe42443276056aaeaa7
sergey <sashumihin@gmail.com>
2021-06-01 17:42:35 +0300

    6й урок. 5 стр.

--- app/src/main/java/com/shumikhin/kotlinkursgb/MainActivity.kt
+++ app/src/main/java/com/shumikhin/kotlinkursgb/MainActivity.kt
@@ -2,10 +2,36 @@ package com.shumikhin.kotlinkursgb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_screen_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_threads -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                        .add(R.id.container, ThreadsFragment.newInstance())
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/shumikhin/kotlinkursgb/ThreadsFragment.kt
@@ -0,0 +1,51 @@
+package com.shumikhin.kotlinkursgb
+
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.fragment.app.Fragment
+import com.shumikhin.kotlinkursgb.databinding.FragmentThreadsBinding
+import java.util.*
+
+class ThreadsFragment : Fragment() {
+
+    private var _binding: FragmentThreadsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentThreadsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener {
+            binding.textView.text = startCalculations(binding.editText.text.toString().toInt())
+            binding.mainContainer.addView(AppCompatTextView(it.context).apply {
+                text = getString(R.string.in_main_thread)
+                textSize = resources.getDimension(R.dimen.main_container_text_size)
+            })
+        }
+    }
+
+    private fun startCalculations(seconds: Int): String {
+        val date = Date()
+        var diffInSec: Long
+        do {
+            val currentDate = Date()
+            val diffInMs: Long = currentDate.time - date.time
+            diffInSec = java.util.concurrent.TimeUnit.MILLISECONDS.toSeconds(diffInMs)
+        } while (diffInSec < seconds)
+        return diffInSec.toString()
+    }
+
+    companion object {
+        fun newInstance() = ThreadsFragment()
+    }
+}
