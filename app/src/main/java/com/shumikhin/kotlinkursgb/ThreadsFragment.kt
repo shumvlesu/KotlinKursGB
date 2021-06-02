6b3ef5a46a9051ab929bab1a895c68660e975fc9
sergey <sashumihin@gmail.com>
2021-06-02 15:50:06 +0300

    6й урок. 14 стр.

--- app/src/main/java/com/shumikhin/kotlinkursgb/ThreadsFragment.kt
+++ app/src/main/java/com/shumikhin/kotlinkursgb/ThreadsFragment.kt
@@ -2,6 +2,8 @@ package com.shumikhin.kotlinkursgb
 
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,8 +17,14 @@ class ThreadsFragment : Fragment() {
 
     private var _binding: FragmentThreadsBinding? = null
     private val binding get() = _binding!!
+    private var counterThread = 0
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentThreadsBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -32,6 +40,52 @@ class ThreadsFragment : Fragment() {
                 textSize = resources.getDimension(R.dimen.main_container_text_size)
             })
         }
+
+        //Приложение может упасть, потому что нельзя
+        //обращаться к элементам UI не из UI-потока.
+        //Используем - activity?.runOnUiThread чтобы сделать приложение потоко безопасным
+        binding.calcThreadBtn.setOnClickListener {
+            Thread {
+                counterThread++
+                val calculatedText = startCalculations(binding.editText.text.toString().toInt())
+                activity?.runOnUiThread {
+                    binding.textView.text = calculatedText
+                    binding.mainContainer.addView(AppCompatTextView(it.context).apply {
+                        text = String.format(getString(R.string.from_thread), counterThread)
+                        textSize = resources.getDimension(R.dimen.main_container_text_size)
+                    })
+                }
+            }.start()
+        }
+
+
+        //HandlerThread
+        // При запуске через HandlerThread все операции выстраиваются в очередь
+        val handlerThread = HandlerThread(getString(R.string.my_handler_thread))
+        handlerThread.start()
+
+        val handler = Handler(handlerThread.looper)
+
+        binding.calcThreadHandler.setOnClickListener {
+
+            binding.mainContainer.addView(AppCompatTextView(it.context).apply {
+                text = String.format(getString(R.string.calculate_in_thread), handlerThread.name)
+                textSize = resources.getDimension(R.dimen.main_container_text_size)
+            })
+
+            handler.post {
+                startCalculations(binding.editText.text.toString().toInt())
+                binding.mainContainer.post {
+                    binding.mainContainer.addView(AppCompatTextView(it.context).apply {
+                        text = String.format(getString(R.string.calculate_in_thread), Thread.currentThread().name)
+                        textSize = resources.getDimension(R.dimen.main_container_text_size)
+                    })
+                }
+            }
+
+        }
+
+
     }
 
     private fun startCalculations(seconds: Int): String {
